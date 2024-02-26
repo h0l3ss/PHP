eb40b091b557aeea51f5bd46df82be9e4667f4b3
Weverthon Gimenes <58565098+h0l3ss@users.noreply.github.com>
2024-02-26 11:05:35 -0300

    Update DBconn.php

--- DBconn.php
+++ DBconn.php
@@ -1,19 +1,1 @@
-<?php
 
-$servername = "xxx";
-$database = "xxx";
-$username = "xxx";
-$password = "xxx";
-
-// Create connection
-
-$conn = mysqli_connect($servername, $username, $password, $database);
-mysqli_set_charset($conn,"utf8");
-
-// Check connection
-
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-
-?>
